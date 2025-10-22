9512f1a4b9481e3a7b2e0574c7a3dd93981e5561
EvOCRO Project <evocroproject@gmail.com>
2025-10-22 15:26:07 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   site: 'https://EvOCROproject.github.io/odyssey-theme/',
   base: '/odyssey-theme/',
-  outDir: './docs',          // Pages -> Deploy from branch -> /docs
   integrations: [lit(), icon(), mdx()],
 });
