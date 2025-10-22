83b3639d1c13681ff922f65b3fabf5034e8eacd7
EvOCRO Project <evocroproject@gmail.com>
2025-10-22 15:04:31 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
-import { defineConfig } from 'astro/config';
-// Se nel tuo progetto usi Lit (pare di sì), tieni l'integrazione:
-import lit from '@astrojs/lit';
-
 export default defineConfig({
   site: 'https://EvOCROproject.github.io/odyssey-theme/',
   base: '/odyssey-theme/',
-  integrations: [lit()],
+  outDir: './docs',
 });
