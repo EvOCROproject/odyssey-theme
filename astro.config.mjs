35e24bc852224eea3021f4cc6141b9899bb7ab04
EvOCRO Project <evocroproject@gmail.com>
2025-10-22 15:19:19 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import lit from '@astrojs/lit';
-import astroIcon from 'astro-icon';
+import icon from 'astro-icon';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://EvOCROproject.github.io/odyssey-theme/',
   base: '/odyssey-theme/',
-  outDir: './docs',
-  integrations: [lit(), astroIcon()],
+  outDir: './docs',          // Pages -> Deploy from branch -> /docs
+  integrations: [lit(), icon(), mdx()],
 });
