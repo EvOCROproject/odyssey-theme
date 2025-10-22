0662e8cc3a7cc992057d78450270a3be8e74eb54
EvOCRO Project <evocroproject@gmail.com>
2025-10-22 23:50:13 +0100

    Update nav.js

--- src/config/nav.js
+++ src/config/nav.js
@@ -1,6 +1,6 @@
 export const nav = [
   { title: 'Home',    slug: '/odyssey-theme/' },
   { title: 'Project', slug: '/odyssey-theme/project' },
-  { title: 'News',    slug: '/odyssey-theme/blog' }, // blog = hub per news/eventi/vlog/press
+  { title: 'News',    slug: '/odyssey-theme/news' }, // blog = hub per news/eventi/vlog/press
   { title: 'Contact', slug: '/odyssey-theme/contact' },
 ];
