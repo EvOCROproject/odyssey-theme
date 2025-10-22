dd955dd5380ebf7d6e7b606cab4a44e02c8a11f8
EvOCRO Project <evocroproject@gmail.com>
2025-10-22 14:37:47 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
-import icon from "astro-icon";
-import lit from "@astrojs/lit";
+// Se nel tuo progetto usi Lit (pare di sì), tieni l'integrazione:
+import lit from '@astrojs/lit';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.sapling.supply/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  sitemap: true, // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
+  site: 'https://EvOCROproject.github.io/odyssey-theme/',
+  base: '/odyssey-theme/',
+  integrations: [lit()],
 });
