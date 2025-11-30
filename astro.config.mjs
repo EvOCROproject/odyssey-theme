0845c62a739711aa46b191f5ba1eeecf778fa02f
EvOCRO Project <evocroproject@gmail.com>
2025-11-30 01:18:02 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import astroIcon from 'astro-icon'; // ⬅️ questo è il plugin che manca
 
 export default defineConfig({
   site: 'https://evocroproject.github.io',
@@ -8,5 +9,6 @@ export default defineConfig({
   integrations: [
     mdx(),
     sitemap(),
+    astroIcon(),   // ⬅️ reintrodotto
   ],
 });
