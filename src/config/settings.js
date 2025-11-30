1b669f80af5b2455a5e0d40ea03547728adcc9bf
EvOCRO Project <evocroproject@gmail.com>
2025-11-30 01:37:59 -0600

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
   title: 'EvOCRO',
   description: 'Evolutionary Origins of Cognition in Crocodiles',
-  url: 'https://EvOCROproject.github.io/odyssey-theme', // no trailing slash
+  url: 'https://EvOCROproject.github.io', // no trailing slash
   name: 'EvOCRO',
   enableThemeSwitcher: true,
   showPlug: false, // 👈 disattiva il plug in footer
