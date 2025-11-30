c2b0401838214abd8287d022d1bb58083d2d534d
EvOCRO Project <evocroproject@gmail.com>
2025-11-30 01:13:55 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-// importa qui le altre integrazioni che avevi prima, se ci sono
-// es: import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://evocroproject.github.io',
-  base: '/',
-  integrations: [
-    mdx(),
-    sitemap(),
-    // tailwind(), ecc. se già presenti
-  ],
+  site: 'https://evocroproject.github.io/odyssey-theme',
+  base: '/odyssey-theme/',
+  // ...
 });
