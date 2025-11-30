ba84a2d2685af5f4cf7d04d2023c2403edf4a38a
EvOCRO Project <evocroproject@gmail.com>
2025-11-30 01:23:13 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import astroIcon from 'astro-icon'; // ⬅️ questo è il plugin che manca
+import lit from '@astrojs/lit'; // ⬅️ integrazione per ThemeSwitcher
 
 export default defineConfig({
   site: 'https://evocroproject.github.io',
